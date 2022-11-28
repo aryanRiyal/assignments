4e82548daa810d63ccd9c6dbf82bd993284b5c88
aryanRiyal <aryan13.riyal@gmail.com>
2022-11-28 11:04:49 +0530

    first commit

---
+++ c_Assignment/printNLines.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char ** firstnumlines(FILE * inputfp, unsigned int num){
+	char ** headbuff = (char **) calloc(num, sizeof(char *));
+
+	for(unsigned int i = 0; i < num; i++){
+		headbuff[i] = (char *) calloc(255, sizeof(char));
+		fgets(headbuff[i],255,inputfp);
+	}
+	return headbuff;
+}
+
+
+
+int main() {
+	FILE * inputfp = fopen("input.txt", "r");
+
+	int num=10;
+	char ** firstlines = firstnumlines(inputfp,num);
+
+	for(int i=0;i<num;i++){
+		if(firstlines[i] != NULL){
+			printf("%s",firstlines[i]);
+		}
+	}
+	// printf("\n");
+	fclose(inputfp);
+	free(firstlines);
+}
